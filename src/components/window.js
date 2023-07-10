4cea2c05b2a0425d1539d551be9769e132dc8c23
jgpentz <jpentz1@gmail.com>
2023-07-10 01:04:49 -0600

    Added a callback function for closing the window.

--- src/components/window.js
+++ src/components/window.js
@@ -1,8 +1,9 @@
 import cd_audio_icon from 'images/cd_audio_icon.png'
 import '98.css'
 import Draggable from 'react-draggable';
+import { useState } from 'react';
 
-function TitleBar() {
+function TitleBar({closeWindow}) {
     return(
         <div className='title-bar'>
             <div class='title-bar-text' style={{fontSize: 14}}>
@@ -12,17 +13,25 @@ function TitleBar() {
             <div className="title-bar-controls">
                 <button aria-label='Minimize'></button>
                 <button aria-label='Maximize'></button>
-                <button aria-label='Close'></button>
+                <button onClick={closeWindow} aria-label='Close'></button>
             </div>
         </div>
     );
 } 
 
 export default function Window() {
-    return(    
+    // Create a state variable for closing the window
+    const [showWindow, setShowWindow] = useState(true);
+
+    // const closeWindow = useCallback(() => setShowWindow(false), [showWindow])
+
+    if (!showWindow) {
+        return null;
+    }
+    return(
         <Draggable defaultClassName="window" handle=".title-bar" bounds="parent">
             <div class="window" style={{width: '600px', height: '400px'}}>
-                <TitleBar />
+                <TitleBar closeWindow={() => setShowWindow(false)} />
                 <div class='window-body'>
                     <p style={{fontSize: 12}}>Room for activites</p>
                 </div>
