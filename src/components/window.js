1759fb4ad1266831c43d94746862cd5afbccfa81
jgpentz <jpentz1@gmail.com>
2023-07-15 17:59:15 -0600

    Inactive window alert calls a callback function from useEffect.

--- src/components/window.js
+++ src/components/window.js
@@ -1,19 +1,19 @@
 import '98.css'
 import Draggable from 'react-draggable';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 import TitleBar from './titlebar.js';
 
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-function useOutsideAlerter(ref, setActiveWindow) {
+function useOutsideAlerter(ref, setAlert) {
     useEffect(() => {
       /**
        * Alert if clicked on outside of element
        */
       function handleClickOutside(event) {
         if (ref.current && !ref.current.contains(event.target)) {
-          setActiveWindow(false);
+            setAlert(false);
         }
       }
       // Bind the event listener
@@ -22,7 +22,7 @@ function useOutsideAlerter(ref, setActiveWindow) {
         // Unbind the event listener on clean up
         document.removeEventListener("mousedown", handleClickOutside);
       };
-    }, [ref, setActiveWindow]);
+    }, [ref, setAlert]);
   }
   
 
@@ -32,8 +32,12 @@ export default function Window({title, defaultPos}) {
     const [minimizeWindow, setMinimizeWindow] = useState(false);
     const [activeWindow, setActiveWindow] = useState(false)
     
+    // set the window as inactive when clicked outside
     const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef, setActiveWindow);
+    const setAlert = useCallback((state) => {
+        setActiveWindow(state)
+    }, []);
+    useOutsideAlerter(wrapperRef, setAlert);
 
     // Bring this window to the front of the screen and updates
     // the titlebar to show that this window is active
